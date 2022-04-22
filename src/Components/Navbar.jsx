768d29b149a96a2aa259001818f3e0465b1170b8
Joseph Ellsberry <jellsberry147@gmail.com>
2022-04-22 09:25:23 -0700

    v1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export function Navbar() {
   return (
     <nav className="topnav">
-      <Link to="/jellsberry-portfolio">Home</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/Home">Home</Link>
+      <Link to="/Contact">Contact</Link>
       <Link to="/About">About</Link>
       <Link to="/Hobbies">Hobbies</Link>
     </nav>
